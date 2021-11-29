2a8f7720451219a76b2b82a6200e9e515a96f46f
asheniac <asheniac@gmail.com>
2021-11-29 23:10:11 +1100

    Grid and Products added

--- nextjs-shopify/components/ui/Grid/Grid.tsx
+++ nextjs-shopify/components/ui/Grid/Grid.tsx
@@ -1,8 +1,19 @@
 import { FC, ReactNode } from "react";
 import styles from "./Grid.module.css";
+import cn from "classnames";
 
-const Grid: FC = ({ children }) => {
-  return <div className={styles.root}>{children}</div>;
+interface Props {
+  children: ReactNode[];
+  layout?: "A" | "B";
+}
+
+const Grid: FC = ({ children, layout = "A" }) => {
+  const rootClassName = cn(styles.root, {
+    [styles.layoutA]: layout === "A",
+    [styles.layoutB]: layout === "B",
+  });
+
+  return <div className={rootClassName}>{children}</div>;
 };
 
 export default Grid;
